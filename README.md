a9546e1969c57c235b80c323a75b4e9f03713d52
Will Carhart <carhartwill@gmail.com>
2021-07-03 07:59:28 -0600

    Working deploy for now

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Memorial site for Reggie.
 
 ### Deploy
-Simple Express app deployed via DigitalOcean. To deploy, see: https://www.digitalocean.com/community/tutorials/how-to-set-up-a-node-js-application-for-production-on-ubuntu-20-04
+Simple Express app deployed via DigitalOcean. To deploy, see `deploy.sh`.
 
 ### About
 Express app is in `index.js`. Utilizes `googledrive.js` to pull images from Google Drive folder and caches locally. Serves public front-end out of `public/`.
---
+++ deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# THIS IS A LOOSE SET OF STEPS, NOT A WORKING SCRIPT
+
+# create user
+sudo useradd -m weg
+sudo passwd weg
+sudo usermod --shell /bin/bash weg
+sudo usermod -aG sudo weg
+sudo su weg
+
+# install node + yarn
+# get NVM from here: https://github.com/nvm-sh/nvm
+nvm --version
+nvm ls-remote
+nvm install 15.14.0
+npm install --global yarn
+
+# open port access for node
+sudo apt-get install libcap2-bin
+sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
+
+# set up app
+mkdir ~/code && cd ~/code
+git clone https://github.com/wcarhart/bigweg.git
+cd bigweg
+mkdir images
+./ssl.sh
+yarn install
+# get credentials.json from GCP
+node index.js # should run app
+
+# set up service
+npm install pm2@latest -g
+pm2 --version
+pm2 start index.js
+pm2 startup systemd
+sudo env PATH=$PATH:/home/weg/.nvm/versions/node/v15.14.0/bin /home/weg/.nvm/versions/node/v15.14.0/lib/node_modules/pm2/bin/pm2 startup systemd -u weg --hp /home/weg
+pm2 save
+sudo systemctl start pm2-weg # if this fails, do `sudo reboot` and rerun this step and then continue
+systemctl status pm2-weg
+# may have to do `pm2 start index.js` again to pull things back up
+
+# should probably set up NGINX as reverse proxy: https://www.digitalocean.com/community/tutorials/how-to-set-up-a-node-js-application-for-production-on-ubuntu-20-04
+# need to fix SSL
+# don't forget to configure DNS
